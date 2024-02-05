36dc117bef3bfd7047bf955d8c7f3b5dcf129358
mohammed shafin <shafinms21@gmail.com>
2024-02-06 01:09:44 +0530

    user log completed

--- controller/userController/signupcontroller.js
+++ controller/userController/signupcontroller.js
@@ -1,5 +1,6 @@
 
 const User = require('../../models/userregistration');
+const bcrypt = require('bcrypt');
 
 
 module.exports = {
@@ -27,4 +28,26 @@ module.exports = {
       console.log('error adding user', err);
     }
   },
+
+  postLogin: async (req, res) => {
+    const {email, password} = req.body;
+    try {
+      const existinguser = await User.findOne({email});
+      if (!existinguser) {
+        return res.status(404).json({message: 'user not found'});
+      }
+
+      const passwordMatch = await bcrypt.compare(
+          password, existinguser.password);
+      if (!passwordMatch) {
+        return res.status(401).json({message: 'password not match'});
+      }
+    } catch (error) {
+      console.log('error in login', error);
+    }
+    res.status(200).json({message: 'user loggd'});
+    console.log('post login', req.body);
+  },
 };
+
+
--- router/user/userRouting.js
+++ router/user/userRouting.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 // eslint-disable-next-line max-len
-const {postSignup, postVerifyOtp} = require('../../controller/userController/signupcontroller');
+const {postSignup, postVerifyOtp, postLogin} = require('../../controller/userController/signupcontroller');
 const SendOtp = require('../../middleware/verification/otpverification');
 const verityOtp = require('../../middleware/verification/verifyotp');
 const validation = require('../../middleware/validation/signupvalidation');
@@ -13,6 +13,7 @@ const validation = require('../../middleware/validation/signupvalidation');
 
 router.post('/usersignup', validation, SendOtp, postSignup);
 router.post('/verifyotp', verityOtp, postVerifyOtp);
+router.post('/userlogin', postLogin);
 
 module.exports = router;
 
