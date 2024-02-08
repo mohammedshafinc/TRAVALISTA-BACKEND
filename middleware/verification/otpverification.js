734f4eb2599a34fa8afd54fd9041f298f3044890
mohammed shafin <shafinms21@gmail.com>
2024-02-08 12:45:11 +0530

    homepage edited

--- middleware/verification/otpverification.js
+++ middleware/verification/otpverification.js
@@ -4,12 +4,7 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-/**
- * Sends an OTP (One-Time Password) using Twilio.
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- */
+
 async function SendOtp(req, res, next) {
   console.log('sunnni');
   const phoneNumber = req.body.mobileNumber;
