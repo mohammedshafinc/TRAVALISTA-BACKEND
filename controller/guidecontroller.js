c86a4f856036529749ff7b45222b5be4459061c7
mohammed shafin <shafinms21@gmail.com>
2024-02-26 18:45:53 +0530

    guide added to databse

--- controller/guidecontroller.js
+++ controller/guidecontroller.js
@@ -2,16 +2,41 @@
 // const jwt = require('jsonwebtoken');
 // const multer = require('multer');
 
+const Guide = require('../models/guideregistration');
+
 
 module.exports = {
-  postGuide: (req, res)=>{
+  postGuide: async (req, res)=>{
+    const filePath = req.file.location;
     try {
-      console.log('from guide', req.body);
-      console.log('fuuuuuuu');
-      console.log('hhhhh', req.file);
-      res.json({Message: 'hello'});
+      console.log(req.body);
+      const {
+        fullname,
+        email,
+        mobilenumber,
+        totalexp,
+        location,
+        about,
+      } = req.body;
+      console.log('fffff0', req.file.location);
+      const newGuide = new Guide({
+        fullname,
+        email,
+        phonenumber: mobilenumber,
+        exp: totalexp,
+        files: filePath,
+        location: location,
+        about,
+      });
+      await newGuide.save();
+      console.log('user added succesfully');
+      console.log(newGuide);
+      res.status(200).json({
+        newGuide,
+        message: 'user added suucesfully',
+      });
     } catch (err) {
-      console.log('error in guide singup', err);
+      console.log('error in guide signup to database', err);
     }
   },
 };
---
+++ models/guideregistration.js
@@ -0,0 +1,46 @@
+const moongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+const guideSchema = new moongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phonenumber: {
+    type: Number,
+    required: true,
+
+  },
+  exp: {
+    type: Number,
+    required: true,
+  },
+  files: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    required: true,
+  },
+});
+// guideSchema.pre('save', async function(next) {
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(this.password, salt);
+//     this.password = hashedPassword;
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+const Guide = moongoose.model('guideregister', guideSchema);
+module.exports = Guide;
